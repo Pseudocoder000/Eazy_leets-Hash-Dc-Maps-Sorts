8e6b15ded85e5f8c2d529104716e158fb04c2e83
Pseudocoder <110370085+Pseudocoder000@users.noreply.github.com>
2025-07-18 01:31:43 +0530

    Time: 0 ms (100%), Space: 21.8 MB (38.56%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n= nums.size();
-        int exsum = (n*(n+1))/2;
-        int sum =0;
-        for(int i=0; i<n; i++){
-            sum += nums[i];
+        sort(nums.begin(),nums.end());
+        if(nums[0]!=0){
+            return 0;
         }
-            return exsum-sum;
-
-        
+        for(int i=1; i<nums.size(); i++){
+            if (nums[i-1]+1 != nums[i]){
+                return nums[i-1]+1;
+            }
+        }
+        return nums[nums.size()-1] +1;
     }
 };
